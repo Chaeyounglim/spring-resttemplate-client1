17c690f16e9f9f2cfb12fcedbc705d356626d134
chae0 <limchae098@gmail.com>
2023-06-20 14:50:18 +0900

    init commit

---
+++ src/main/java/com/sparta/springresttemplateclient1/SpringResttemplateClient1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springresttemplateclient1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResttemplateClient1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringResttemplateClient1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springresttemplateclient1/SpringResttemplateClient1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springresttemplateclient1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResttemplateClient1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
